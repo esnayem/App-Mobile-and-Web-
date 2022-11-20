b12a83f5ae1fae9370eadee0adc6a3e5ecbd300a
Nayem <esnayemmolla@gmail.com>
2022-11-20 11:21:34 +0600

    timer update for splashScreen

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     // TODO: implement initState
-    Timer(Duration(seconds: 1),(){
+    Timer(Duration(seconds: 3),(){
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>homePage()));
     });
     super.initState();
